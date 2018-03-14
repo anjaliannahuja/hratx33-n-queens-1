7ac6cc12f9e52515c6bf5f70cfd3f0d099b0d807
Hack Reactor Student <student@hackreactor.com>
2018-03-14 12:27:54 -0500

    Implementing bitwise logic

--- src/solvers.js
+++ src/solvers.js
@@ -14,12 +14,65 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+/**
+ * @param {Number} n - Size of nxn chessboard
+ * 
+ * @description
+ * 1. Set the initialth position of a binary number "board" to startBoard + left shift initial
+ *      Ex. startBoard = 1, initial = 5 
+ *      board = 0000000000000001 + 0000000000100000
+ * 2. Calculate first position which can be occupied:
+ *    If this value is greater than n ** 2, return the board
+ * 3. Add 1 << positon to occupy to board in order to fill the position
+ * 4. Recursively call findNRooksSolution with n and board
+ *       
+ */
+window.findNRooksSolution = function(n, startBoard = 1, initial = 0) {
+  let board = startBoard + (1 << initial);
+  let binaryString = '';
+  //Put binary representation of board into binaryString
+  const findBinaryRepresentation = number => {
+    if (number === 0) return undefined;
+    binaryString += '' + (number % 2);
+    findBinaryRepresentation(Math.floor(number/2));
+  }
+  findBinaryRepresentation(board);
+  //Get index + 1 of every bit in the binaryString, put those into binaryPositions
+  const findUnsafePositions = binaryString => {
+    let flippedString = binaryString.reverse();
+    //Positions with a 1
+    const bitPositions = [];
+    for (let stringIndexPos = 0; stringIndexPos < flippedString.length; stringIndexPos++) {
+      if (flippedString[stringIndexPos] === 1) bitPositions.push(stringIndexPos + 1);
+    }
+    //Calculate unsafe positions for each bit in bitPositions, then sort the unsafe positions
+    //and remove duplicates
+    const unsafePositions = [];
+    for (const position of bitPositions) {
+      
+    }
+  }
+  const unsafePositions = findUnsafePositions(binaryString);
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  //Calculate next available position for rook
+  const findNextPosition = unsafePositions => {
+    return unsafePositions.reduce((availablePositon, takenPosition, index) => {
+      //If next available position has not been found
+      if (availablePositon === -1) {
+        //If the next position equals this position + 1, then the next bit is not safe
+        if (takenPosition + 1 !== unsafePositions[index + 1]) {
+          availablePositon = takenPosition + 1;
+          return availablePositon;
+        }
+      }
+      return availablePositon;
+    }, -1);
+  }
+  const positionToOccupy = findNextPosition(unsafePositions);
+  board += (1 << (positionToOccupy - 1));
+  
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return board;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
